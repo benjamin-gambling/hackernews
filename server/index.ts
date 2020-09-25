ec76a936f02a412e3f511b6a447cdaadf60e06ce
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-09-25 12:21:58 -0700

    cant find route so edited file path

--- server/index.ts
+++ server/index.ts
@@ -35,16 +35,16 @@ const server = new GraphQLServer({
   },
 });
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
-let TYPE = "public";
+// let TYPE = "public";
 
-if (process.env.NODE_ENV === "production") TYPE = "build";
+// if (process.env.NODE_ENV === "production") TYPE = "build";
 
-server.express.use(express.static(`../${TYPE}`));
+// server.express.use(express.static(`../${TYPE}`));
 
-server.express.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, `../${TYPE}", "index.html`));
-});
+// server.express.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, `../${TYPE}", "index.html`));
+// });
 
 server.start(() => console.log(`Server started on port ${PORT}`));
