50c087274ef43d638fe00502e941c4cc085b7c6d
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-09-26 16:41:00 -0700

    alter structure

--- server/index.ts
+++ server/index.ts
@@ -40,7 +40,7 @@ server.express.use(compression());
 
 const PORT = process.env.PORT || 4000;
 
-server.express.use("/", express.static(`/../build`));
+server.express.use("/", express.static(path.join(__dirname, `/../build`)));
 
 server.express.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname + `/../build/index.html`));
